e52ef851a35db7043f4812309a65339515df8044
Valibert <valibert.b@gmail.com>
2023-02-15 21:45:06 +0100

    Finish design and add github tag

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./reset.css";
 import arrowUp from "./arrow-up.svg";
 import arrowDown from "./arrow-down.svg";
 import bin from "./bin.svg";
+import github from "./github.svg";
 
 function App() {
   return (
@@ -13,31 +14,32 @@ function App() {
           <li>
             <h3>#1 Task title</h3>
             <p>Task details</p>
-            <img src={arrowUp} alt="arrow-up" className="arrow-up" />
-            <img src={arrowDown} alt="arrow-down" className="arrow-down" />
-            <img src={bin} alt="bin" className="bin" />
+            <img src={arrowUp} alt="arrow-up" />
+            <img src={arrowDown} alt="arrow-down" />
+            <img src={bin} alt="bin" />
           </li>
           <li>
             <h3>#2 Task title</h3>
             <p>Task details</p>
-            <img src={arrowUp} alt="arrow-up" className="arrow-up" />
-            <img src={arrowDown} alt="arrow-down" className="arrow-down" />
-            <img src={bin} alt="bin" className="bin" />
+            <img src={arrowUp} alt="arrow-up" />
+            <img src={arrowDown} alt="arrow-down" />
+            <img src={bin} alt="bin" />
           </li>
           <li>
             <h3>#3 Task title</h3>
             <p>Task details</p>
-            <img src={arrowUp} alt="arrow-up" className="arrow-up" />
-            <img src={arrowDown} alt="arrow-down" className="arrow-down" />
-            <img src={bin} alt="bin" className="bin" />
+            <img src={arrowUp} alt="arrow-up" />
+            <img src={arrowDown} alt="arrow-down" />
+            <img src={bin} alt="bin" />
           </li>
         </ul>
       </div>
-      <form action="submit">
+      <form action="submit" className="new-task">
         <h2>Create new task</h2>
         <input type="text" placeholder="Title" />
         <input type="text" placeholder="Details" />
         <button>Add</button>
+        <p className="tag"><img src={github} alt="github" /> Built by <a href="#">Valentin B.B.</a></p>
       </form>
     </div>
   );
